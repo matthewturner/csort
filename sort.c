b8ce1b1c619f2e8cdc1b24806a5ce48115ccd225
Matt Turner <matt@fiverings.co.uk>
2020-06-08 20:17:15 +0100

    Add call to qsort

--- sort.c
+++ sort.c
@@ -1,21 +1,49 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+typedef char InputString[65536];
+typedef char InputLine[10];
+typedef char * LinePtr;
 
 int main()
 {
-    if (!isatty(fileno(stdin)))
+    if (isatty(fileno(stdin)))
     {
-        int i = 0;
-        char pipe[65536];
-        while (-1 != (pipe[i++] = getchar()))
-            ;
-        pipe[i - 3] = '\0';
+        return 0;
+    }
 
-        char *split = strtok(pipe, "\n");
-        while (split != NULL)
-        {
-            fprintf(stdout, "%s\n", split);
-            split = strtok(NULL, "\n");
-        }
+    size_t i = 0;
+    InputString input;
+    while (-1 != (input[i++] = getchar()))
+        ;
+    input[i - 1] = '\0';
+
+    LinePtr linePtr = strtok(input, "\n");
+    LinePtr originalLinePtr = linePtr;
+    int counter = 0;
+    while (linePtr != NULL)
+    {
+        fprintf(stdout, "%s\n", linePtr);
+        linePtr = strtok(NULL, "\n");
+        counter++;
+    }
+
+    InputLine * lines = (InputLine*)calloc(counter, sizeof(InputLine));
+    for(int i = 0; i < counter; i++)
+    {
+        strcpy(lines[i], originalLinePtr);
+        originalLinePtr++;
+    }
+
+    fprintf(stdout, "Count: %d\n", counter);
+
+    qsort(lines, counter, 10, strcmp);
+
+    fprintf(stdout, "Sorted!\n");
+
+    for(int i = 0; i < counter; i++)
+    {
+        fprintf(stdout, "%s\n", lines[i]);
     }
 }
